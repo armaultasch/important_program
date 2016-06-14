b7ea97d4125867f60c9d5237f3757e3b161d9e12
Alison Maultasch <armaultasch@gmail.com>
2016-06-14 12:21:13 -0400

    calling other files to perform tasks

---
+++ app.rb
@@ -0,0 +1,16 @@
+require_relative("lib/cars.rb")
+# require_relative("lib/spaceship.rb")
+
+porsche = Car.new("Broom", 2)
+
+porsche.noise
+
+bmw = Car.new("BROOOOOOM", 5)
+
+bmw.noise
+
+# boeing = SpaceShip.new("Woooosh", 50)
+# boeing.fly
+
+# boeing = SpaceShip.new("Brrrrmm", 100)
+# boeing.fly
