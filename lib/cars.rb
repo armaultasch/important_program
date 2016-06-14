287b51b27d8aa8e27a9b36ec4e879169378bff28
Alison Maultasch <armaultasch@gmail.com>
2016-06-14 12:22:31 -0400

    car file

---
+++ lib/cars.rb
@@ -0,0 +1,10 @@
+class Car
+	def initialize(sound, capacity)
+		@sound = sound
+		@capacity = capacity
+	end
+
+	def noise
+		puts @sound, @capacity
+	end
+end
---
+++ lib/spaceship.rb
@@ -0,0 +1,10 @@
+class SpaceShip
+	def initialize(sound, capacity)
+		@sound = sound
+		@capacity = capacity
+	end
+
+	def fly
+		puts @sound
+	end
+end
